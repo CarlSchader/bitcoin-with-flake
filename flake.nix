c9f8da6d4b6bb7f013c0f2f498a727ac0a5547dd
Carl Schader <carlschader@gmail.com>
2025-06-15 21:05:11 -0400

    fixed the package

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, flake-utils, ... }:
+  outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
@@ -45,52 +45,52 @@
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or https://opensource.org/license/mit/.
 
-          include_guard(GLOBAL)
+include_guard(GLOBAL)
 
 # Illumos/SmartOS requires linking with -lsocket if
 # using getifaddrs & freeifaddrs.
-          function(test_append_socket_library target)
-            if (NOT TARGET ''${target})
-              message(FATAL_ERROR "''${CMAKE_CURRENT_FUNCTION}() called with non-existent target \"''${target}\".")
-            endif()
+function(test_append_socket_library target)
+  if (NOT TARGET ''${target})
+    message(FATAL_ERROR "''${CMAKE_CURRENT_FUNCTION}() called with non-existent target \"''${target}\".")
+  endif()
 
-            set(check_socket_source "
-              #include <sys/types.h>
-              #include <ifaddrs.h>
+  set(check_socket_source "
+    #include <sys/types.h>
+    #include <ifaddrs.h>
 
-              int main() {
-                struct ifaddrs* ifaddr;
-                getifaddrs(&ifaddr);
-                freeifaddrs(ifaddr);
-              }
-            ")
+    int main() {
+      struct ifaddrs* ifaddr;
+      getifaddrs(&ifaddr);
+      freeifaddrs(ifaddr);
+    }
+  ")
 
-            include(CheckCXXSourceCompiles)
-            check_cxx_source_compiles("''${check_socket_source}" IFADDR_LINKS_WITHOUT_LIBSOCKET)
-            if(NOT IFADDR_LINKS_WITHOUT_LIBSOCKET)
-              include(CheckSourceCompilesWithFlags)
-              check_cxx_source_compiles_with_flags("''${check_socket_source}" IFADDR_NEEDS_LINK_TO_LIBSOCKET
-                LINK_LIBRARIES socket
-              )
-              if(IFADDR_NEEDS_LINK_TO_LIBSOCKET)
-                target_link_libraries(''${target} INTERFACE socket)
-              else()
-                message(FATAL_ERROR "Cannot figure out how to use getifaddrs/freeifaddrs.")
-              endif()
-            endif()
-            set(HAVE_IFADDRS TRUE PARENT_SCOPE)
-          endfunction()
+  include(CheckCXXSourceCompiles)
+  check_cxx_source_compiles("''${check_socket_source}" IFADDR_LINKS_WITHOUT_LIBSOCKET)
+  if(NOT IFADDR_LINKS_WITHOUT_LIBSOCKET)
+    include(CheckSourceCompilesWithFlags)
+    check_cxx_source_compiles_with_flags("''${check_socket_source}" IFADDR_NEEDS_LINK_TO_LIBSOCKET
+      LINK_LIBRARIES socket
+    )
+    if(IFADDR_NEEDS_LINK_TO_LIBSOCKET)
+      target_link_libraries(''${target} INTERFACE socket)
+    else()
+      message(FATAL_ERROR "Cannot figure out how to use getifaddrs/freeifaddrs.")
+    endif()
+  endif()
+  set(HAVE_IFADDRS TRUE PARENT_SCOPE)
+endfunction()
 
 # Always link with libatomic on NixOS
-          function(test_append_atomic_library target)
-            if (NOT TARGET ''${target})
-              message(FATAL_ERROR "''${CMAKE_CURRENT_FUNCTION}() called with non-existent target \"''${target}\".")
-            endif()
+function(test_append_atomic_library target)
+  if (NOT TARGET ''${target})
+    message(FATAL_ERROR "''${CMAKE_CURRENT_FUNCTION}() called with non-existent target \"''${target}\".")
+  endif()
 
-            message(STATUS "NixOS detected - forcing linking with libatomic")
-            target_link_libraries(''${target} INTERFACE atomic)
-          endfunction()
-          EOL
+  message(STATUS "NixOS detected - forcing linking with libatomic")
+  target_link_libraries(''${target} INTERFACE atomic)
+endfunction()
+EOL
           '';
 
           meta = with pkgs.lib; {
